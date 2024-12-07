767eceb42e68a9a416c6a198edbbf2e76dfa4f2c
Omar Salama <omar550205@gmail.com>
2024-12-08 01:09:28 +0200

    some configirations

--- help-me/app/questions/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function page() {
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold">Questions</h2>
-      <p className="mt-2">This is the questions page content.</p>
-    </div>
-  );
-}
